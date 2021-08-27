d08762f2451f1df087e057ae44f5399b9745c13f
Luan Argolo <ipsluan@icloud.com>
2021-08-27 16:52:57 -0300

    :sparkles: Show movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './tmdb';
+import MovieRow from './components/MovieRow';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default() => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+      setMovieList(list);
+    }
+
+    loadAll();
+
+  }, []);
+
   return (
-    <div>
-      Olá mundo
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
