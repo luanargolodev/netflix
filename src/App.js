1221e821bb53001f940574fa0cc50bec1ea2c48a
Luan Argolo <ipsluan@icloud.com>
2021-08-28 08:51:10 -0300

    :sparkles: Added transition with scroll

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 export default () => {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -26,9 +27,24 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+
+    return() => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
-      <Header />
+      <Header black={blackHeader}/>
 
       {featuredData && 
         <FeaturedMovie item={featuredData}/>
